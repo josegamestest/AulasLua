fdf7b11ef0000f029dee965625a97ea482dc24f5
josegamestest <joseanastaciocastilho@gmail.com>
2021-01-20 18:12:52 -0300

    Atualisando o Algoritimo

---
+++ exercicioslua/calculoprejuizo.lua
@@ -0,0 +1,17 @@
+semanapassada=200
+anteontem = 100
+ontem = 80
+
+valor = anteontem-ontem
+if valor < anteontem then
+    print("O valor de anteontem comparado a anteontem deu lucro de ", valor)
+else
+    print("O valor de anteontem comparado a anteontem deu pejuizo de", valor)
+end
+
+valo2 = semanapassada-anteontem
+if valo2 >anteontem then
+    print("O valor de semanapassada comparado a anteontem deu lucro de media de", valo2)
+else
+    print("O valor de semanapassada comparado a anteontem deu prejuizo de", valo2)
+end
