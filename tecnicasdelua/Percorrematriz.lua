737b2a6871914368f52e01895447e10e698ff2bd
Jose Anastacio <76887321+josegamestest@users.noreply.github.com>
2021-11-03 10:38:46 -0300

    criacao do arquivp
    Pasta e Arquivo criado

---
+++ tecnicasdelua/Percorrematriz.lua
@@ -0,0 +1,10 @@
+--Percorrer matriz em lua
+local vetor2={{"itemA"},
+              {"itemB"},
+              {"itemC"},
+              {"itemD"},
+              {"itemE"},
+              }
+for i=1, #vetor2, 1 do
+    print(vetor2[i][1]);
+  end 
