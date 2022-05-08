a4523cd9d6f572c648c4761c6224907ae9b5d326
joseanastacio <joseanastaciocastilho@gmail.com>
2022-05-08 11:36:19 -0300

    vetores e matrizes

--- exercicioslua/vetor_e_matriz_lua.sh
+++ exercicioslua/vetor_e_matriz_lua.sh
@@ -3,8 +3,16 @@
 -- faz o comando chmood +x com nome do arquivo para torna-lo executavel
 -- ./nomedoarquivo.sh para executalo
 
---vetor lua
-
-test={"jose","anastacio"}
+-- vetor lua unidimenssional
+test={"Primeiro","Segundo"}
 -- concatena e mostra na tela os valores dos dois vetores
 print(test[1].." "..test[2])
+
+--------------------------------------------------------------------------------------------------------------
+-- mostra cada valor do vetor a cada atualisacao do laco "for"
+test2={"Primeiro ","Segundo"}
+for i = 1, #test2, 1 do
+	print(test2[i])
+end
+
+--------------------------------------------------------------------------------------------------------------
