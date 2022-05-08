78dd7c825a83f2412ddbd645ba7787ac953868f8
joseanastacio <joseanastaciocastilho@gmail.com>
2022-05-08 11:12:03 -0300

    vetores e matrizes

---
+++ exercicioslua/vetor_e_matriz_lua.sh
@@ -0,0 +1,10 @@
+#!/bin/lua
+-- cria o arquivo .sh
+-- faz o comando chmood +x com nome do arquivo para torna-lo executavel
+-- ./nomedoarquivo.sh para executalo
+
+--vetor lua
+
+test={"jose","anastacio"}
+-- concatena e mostra na tela os valores dos dois vetores
+print(test[1].." "..test[2])
